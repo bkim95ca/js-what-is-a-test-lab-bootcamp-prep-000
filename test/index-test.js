aed6a78ebd0f93cb0c62c3e84b4890c805894750
Brandon Kim <bkim95ca@gmail.com>
2018-02-06 02:20:50 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -3,14 +3,14 @@ var index = require("../index.js")
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
-      `$expect(index.name).toEqual('Susan')`
+      `expect${index.name}.toEqual('Susan')`
     })
   })
 
 
   describe('Height', () => {
     it('is less than 40', () => {
-      `$expect(index.height).toBeLessThan(40)`
+      `expect${index.height}.toBeLessThan(40)`
     })
   })
 
